9a3540fe0afce7fd88c72e0502a1b43c23a3d89e
vishakha <sq.vishu@gmail.com>
2024-06-25 09:26:55 +0530

    first commit

---
+++ 1-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    cout << "enter the number :";
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+    }
+}
